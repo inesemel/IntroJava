4bde2f37b4f4037bb49bb5a2e2713eb9ee2a2093
inesemel <inese.melehova@inbox.lv>
2022-03-03 20:35:14 +0200

    The homework is added

---
+++ src/com/company/Homework1.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+//        int n ;
+        for (int n = 2; n < 81; n++){
+            int x = 0;
+            for (int j = n; j >= 1; j--){
+                if (n % j == 0){
+                    x = x+1;
+                }
+            } if (x==2){
+                System.out.println(n + " is a prime number");
+            }
+
+        }
+
+
+        char operator = '^'; // you can change an operator
+
+        double a = 25; // you can change numbers
+        double b= 5;
+        double c;
+        if (operator == '+'){
+            c = a + b;
+            System.out.println(c);
+        } else if (operator == '-'){
+            c = a - b;
+            System.out.println(c);
+        } else if (operator == '/'){
+            c = a / b;
+            System.out.println(c);
+        } else if (operator == '*') {
+            c = a * b;
+            System.out.println(c);
+        } else {
+            System.out.println("There is no suitable mathematical operation");
+        }
+
+    }
+}
