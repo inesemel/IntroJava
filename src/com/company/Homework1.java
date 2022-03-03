ac75f2e8529fcf14bdc9c2a3e78d2b1675a02f9b
inesemel <inese.melehova@inbox.lv>
2022-03-03 20:57:29 +0200

    Homework No.1 added

--- src/com/company/Homework1.java
+++ src/com/company/Homework1.java
@@ -14,13 +14,11 @@ public class Homework1 {
             } if (x==2){
                 System.out.println(n + " is a prime number");
             }
-
         }
 
 
         //Task 2: a calculator
         char operator = '^'; // you can change an operator
-
         double a = 25; // you can change numbers
         double b= 5;
         double c;
